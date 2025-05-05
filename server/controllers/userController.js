72a95a8ae7315476eb91e7c17f1e0108f9f79c69
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-05-05 10:11:30 +0530

    expired date to the local user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,12 +38,16 @@ function createUser(req, res) {
 
 function loginLocalUsers(req, res) {
     const { email, password } = req.body;
+    
 
     db.all("SELECT * FROM users WHERE email=? AND password=? LIMIT 1", [email, password], (err, rows) => {
         if (err) {
             res.status(500).send({ message: "Error while logging in" });
         } else if (rows.length > 0) {
-            res.status(200).send({ message: "Login successful", user:{name:rows[0].name,email:rows[0].email,role:rows[0].role} });
+            db.all("SELECT * FROM data", (err1, rows1) => {
+                res.status(200).send({ message: "Login successful", user:{name:rows[0].name,email:rows[0].email,role:rows[0].role ,expire_date:rows1[0].expire_date} });
+            });
+            
         } else {
             res.status(401).send({ message: "Invalid email or password" });
         }
